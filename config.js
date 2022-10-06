c7f2c0018c1127b6453fde185765e1568c0c668d
straigo <straigo.come@gmail.com>
2022-10-06 09:43:09 +0300

    change-server

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
   // baseUrl: "https://project-manager-121212.herokuapp.com/",
-  baseUrl: "http://localhost:5000/api/",
-  // baseUrl: "https://straigo-server.herokuapp.com/api/",
+  // baseUrl: "http://localhost:5000/api/",
+  baseUrl: "https://straigo.onrender.com/api/",
 };
